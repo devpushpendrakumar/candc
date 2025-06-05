f11635e76e12af53cb999e791b8318098ecb5fb4
devpushpendrakumar <devpushpendrakumar@gmail.com>
2025-06-05 11:41:20 +0530

    <Fix>: Fixed the error middleware

--- server/src/middlewares/error.middleware.js
+++ server/src/middlewares/error.middleware.js
@@ -1,12 +1,16 @@
-const apiResponse = (
-  res,
-  { statusCode = 200, message = "Success", data = null, error = null } = {}
-) => {
-  res.status(statusCode).json({
-    status: statusCode,
+import ApiResponse from "../utils/apiResponse.js";
+
+const errorMiddleware = (err, req, res, next) => {
+  console.error("🔥 Error:", err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return ApiResponse(res, {
+    statusCode,
     message,
-    data,
+    error: err.stack, // Optional: remove this in production
   });
 };
 
-export default apiResponse;
+export default errorMiddleware;
